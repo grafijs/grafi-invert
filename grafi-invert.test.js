bf48b0c1482cb46dd07d3cdde7434584466e762b
kosamari <mariko.kosaka@gmail.com>
2016-04-23 20:57:47 +0100

    update test

--- grafi-invert.test.js
+++ grafi-invert.test.js
@@ -4,7 +4,7 @@ var grafi = require('./grafi-invert.js')
 var imageData = grafi.invert({data: [255, 255, 255, 255], width: 1, height: 1})
 
 assert(imageData.constructor.toString().match(/function\s(\w*)/)[1] === 'GrafiImageData',
-  'returned object is an instance of ImageData')
+  'returned object is an instance of GrafiImageData')
 assert(imageData.data[0] === 0, 'color channel 1 is inverted')
 assert(imageData.data[1] === 0, 'color channel 2 is inverted')
 assert(imageData.data[2] === 0, 'color channel 3 is inverted')
